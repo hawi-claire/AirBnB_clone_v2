fb21fe98537548d18181f383a67becb5b251b15e
hawi-claire <7hawi7@gmail.com>
2025-01-25 09:23:01 -0800

    [Update: IP addresses for my new servers]

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
 """
 Deletes out-of-date archives
 fab -f 100-clean_web_static.py do_clean:number=2
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['18.214.88.46', '34.207.237.195']
 
 
 def do_clean(number=0):
